8df1593b8955aa5146459a843fff69a7282a81f1
JongWon <jack603@naver.com>
2023-02-15 17:02:53 +0900

    vanilla JS counter

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const add = document.querySelector('#add');
+const minus = document.querySelector('#minus');
+const number = document.querySelector('span');
+
+let count = 0;
+
+number.innerText = count;
+
+const updateText = () => {
+  number.innerText = count;
+};
+
+add.addEventListener('click', () => {
+  count += 1;
+  updateText();
+});
+
+minus.addEventListener('click', () => {
+  count -= 1;
+  updateText();
+});
