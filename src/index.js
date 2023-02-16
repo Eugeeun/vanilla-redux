3c3e93973d1ca08656ba70c91e9d667ed86b5ffd
JongWon <jack603@naver.com>
2023-02-16 17:09:48 +0900

    using redux

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
+import { createStore } from 'redux';
+
 const add = document.querySelector('#add');
 const minus = document.querySelector('#minus');
 const number = document.querySelector('span');
 
-let count = 0;
-
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
+const countModifier = (count = 0, action) => {
+  if (action.type === 'ADD') {
+    return count + 1;
+  } else if (action.type === 'MINUS') {
+    return count - 1;
+  } else return count;
 };
 
-add.addEventListener('click', () => {
-  count += 1;
-  updateText();
-});
+const countStore = createStore(countModifier);
 
-minus.addEventListener('click', () => {
-  count -= 1;
-  updateText();
-});
+const onChange = second => {
+  console.log(countStore.getState());
+};
+
+countStore.subscribe(onChange);
+
+add.addEventListener('click', () => countStore.dispatch({ type: 'ADD' }));
+minus.addEventListener('click', () => countStore.dispatch({ type: 'MINUS' }));
