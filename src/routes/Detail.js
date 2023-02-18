45176350cad0802cc6057c2384b6403ad5bcbe13
JongWon <jack603@naver.com>
2023-02-19 02:42:46 +0900

    detail page

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 function Detail() {
-  return <div>Detail</div>;
+  const toDos = useSelector(state => state);
+  const id = useParams().id;
+  const currentToDo = toDos.find(toDo => toDo.id === parseInt(id));
+
+  return (
+    <div>
+      <h1>{currentToDo?.text}</h1>
+      <h3>{`created at: ${currentToDo?.id}`}</h3>
+    </div>
+  );
 }
 
 export default Detail;
