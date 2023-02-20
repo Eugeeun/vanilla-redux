71fb6b4a3aeca3474dafac41560270b8a5b23d28
JongWon <jack603@naver.com>
2023-02-20 23:10:13 +0900

    error message debug

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './components/App';
 import store from './store';
 
-ReactDOM.render(
+const root = document.querySelector('#root');
+
+ReactDOM.createRoot(root).render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.querySelector('#root')
+  </Provider>
 );
